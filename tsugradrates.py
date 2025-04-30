dbef74df646900175af1ce46fead81301de041cd
karina7272 <31832684+karina7272@users.noreply.github.com>
2025-04-30 14:19:03 -0500

    Add files via upload

--- tsugradrates.py
+++ tsugradrates.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1vt1adI5Wfya3ASL_8MotEv9oPHWkvTC3
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import numpy as np
